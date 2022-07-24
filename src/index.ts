4f96df3b465e200e12dcdc0ce8cc8c781c5b46d6
viQcinese <vitor.senise@gmail.com>
2022-07-24 16:04:22 -0300

    feat: base server config with express and puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello");
+import express from "express";
+import puppeteer from "puppeteer";
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.nhk.or.jp/");
+
+  const data = await page.evaluate(() => {
+    return {
+      title: document.querySelector("h1")?.innerText,
+    };
+  });
+  await browser.close();
+
+  res.status(200).json(data);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
